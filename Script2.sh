bee6caeab9bb97067cd670c4fd4888ca0c4a57b3
rj-2232 <roshanmine111@gmail.com>
2025-12-06 08:11:08 +0530

    Update Script2.sh

--- Script2.sh
+++ Script2.sh
@@ -12,7 +12,3 @@ echo "Hello value of a is $a and value of b is $b"
 echo "Do you understand the script?"
 echo "Value of a"
 read a
-echo "Value of b"
-read b
-echo "Hello value of a is $a and value of b is $b"
-echo "Do you understand the script?"
